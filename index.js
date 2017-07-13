75fe9f3c908d5ce14491392c108bea24718f12f8
pjvalentini <pj.valentini@gmail.com>
2017-07-12 21:35:28 -0400

    Updates

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ const jsonfilereader = require('./jsonfilereader');
 // arguments passed when the Node.js process was launched.
 const country = process.argv[2];
 
+function leadingCase(str) {
+	return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 // This function takes the file path and a function
 jsonfilereader('./countries.json', function(countries) {
 	countries.forEach(function(item) {
 		if (country === item.name) {
 			for (var key in item) { // eslint-disable-line
-				console.log(`${key}: ${item[key]}`);
+				console.log(`${leadingCase(key)}: ${item[key]}`);
 			}
 		}
 	});
